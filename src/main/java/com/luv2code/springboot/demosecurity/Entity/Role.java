a342b9ab56d2e54b63cfe6167e82c48b68ebc141
Michalis Anastasiadis <51520658+mihh3@users.noreply.github.com>
2025-03-23 15:51:43 +0200

    Role Entity
    Added a Role Entity Class . So far logging in works good with the users stored in the db .

---
+++ src/main/java/com/luv2code/springboot/demosecurity/Entity/Role.java
@@ -0,0 +1,51 @@
+package com.luv2code.springboot.demosecurity.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "role")
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(String userId, String role) {
+        this.userId = userId;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
+
+
