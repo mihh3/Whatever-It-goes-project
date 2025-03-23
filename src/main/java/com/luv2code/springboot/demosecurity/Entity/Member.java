ed3cc90fe31347d0c91ebce89e35ba19dbea467c
Michalis Anastasiadis <51520658+mihh3@users.noreply.github.com>
2025-03-23 14:55:41 +0200

    User Entity Added .
    User Entity Added according to the DB Schema .

---
+++ src/main/java/com/luv2code/springboot/demosecurity/Entity/Member.java
@@ -0,0 +1,54 @@
+package com.luv2code.springboot.demosecurity.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "pw")
+    private String password;
+
+    @Column(name ="active")
+    private Boolean active ;
+
+    public Member() {
+    }
+
+    public Member(String userId, String password, boolean active) {
+        this.userId = userId;
+        this.password = password;
+        this.active = active;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
